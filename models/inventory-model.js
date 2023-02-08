c46410ab1a6806d773d396b317b22c6c732271c8
James Oku <104051229+jamesoku@users.noreply.github.com>
2023-02-08 16:57:44 -0700

    NEW

---
+++ models/inventory-model.js
@@ -0,0 +1,21 @@
+const pool = require("../database");
+
+async function getClassifications() {
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  );
+}
+
+async function getVehiclesByClassificationId(classificationId) {
+  try {
+    const data = await pool.query(
+      "select * From public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.classification_id = $1",
+      [classificationId]
+    );
+    return data.rows;
+  } catch (error) {
+    console.error("getclassificationbyid error" + error);
+  }
+}
+
+module.exports = { getClassifications, getVehiclesByClassificationId };
