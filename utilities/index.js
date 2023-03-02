8f3048f42c17d5e4a4999e77d7261085ac0c4f59
James Oku <104051229+jamesoku@users.noreply.github.com>
2023-03-02 00:18:14 -0700

    new

--- utilities/index.js
+++ utilities/index.js
@@ -65,7 +65,7 @@ Util.buildaddvehicleform = async function (classification_id = null) {
   let data = await invModel.getClassifications();
 
   let classificationList = `<select name= "classification_id"  id="classification_id" required>`;
-  classificationList += "<option value = '0' >Choose a Classification</option>";
+  classificationList += "<option value = '' >Choose a Classification</option>";
 
   data.rows.forEach((row) => {
     classificationList += '<option value=" ' + row.classification_id + '"';
